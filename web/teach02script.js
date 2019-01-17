0fe1bc370162644bb1485ccb65a12baf5572fa3d
maikeru2020 <maikeru2020@gmail.com>
2019-01-16 17:37:04 -0800

    added # for ids

--- web/teach02script.js
+++ web/teach02script.js
@@ -6,8 +6,8 @@ function changeColor(color) {
     document.getElementById("block1").style.color = color;
 }
 $(document).ready(function() {
-    $("backgroundButton").click(function() {
-        newColor = $("color").val;
-        $("block1").attr("background-color", newColor);
+    $("#backgroundButton").click(function() {
+        newColor = $("#color").val();
+        $("#block1").attr("background-color", newColor);
     })
 })
