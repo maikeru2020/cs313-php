10227b6542f4468245659bc01f3c14f890422232
maikeru2020 <maikeru2020@gmail.com>
2019-02-04 13:43:17 -0800

    first attempt

---
+++ web/teach05/retrieve_scripture.php
@@ -0,0 +1,39 @@
+<?php
+
+try
+{
+  $dbUrl = getenv('DATABASE_URL');
+
+  $dbOpts = parse_url($dbUrl);
+
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $ex)
+{
+  echo 'Error!: ' . $ex->getMessage();
+  die();
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Scripture</title>
+</head>
+<body>
+<?php
+    foreach ($db->query('SELECT book, chapter, verse, content FROM note_user') as $row)
+    {
+    echo $row['book'];
+    }
+?>
+</body>
+</html>
